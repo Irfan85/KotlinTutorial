aefd5f8284835ab052c01e23990a380c4c296c61
Irfan Muhit <irfan.muhit85@gmail.com>
2022-06-01 02:55:03 +0600

    Classes-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
 fun main(args: Array<String>) {
     val akkas = Player("Akkas Ali")
     println(akkas)
+    akkas.showWeaponInfo()
 
     val kuddus = Player("Abdul Kuddus", 5, 10)
+    val newWeapon : Weapon = Weapon("Axe", 12)
+    kuddus.weapon = newWeapon
     println(kuddus)
+    kuddus.showWeaponInfo()
 
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -3,9 +3,15 @@ class Player (val name : String, val level : Int = 1) {
     var lives = 3
     val score = 0
 
+    var weapon : Weapon = Weapon("Fist", 1)
+
     // We can also explicitly create secondary constructors if we want
     constructor(name : String, level: Int, lives : Int) : this(name, level) {
         this.lives = lives
+    }
+
+    fun showWeaponInfo() {
+        println("Current Weapon: ${weapon.name}. Damage Inflicted: ${weapon.damageInflicted}.")
     }
 
     override fun toString(): String {
