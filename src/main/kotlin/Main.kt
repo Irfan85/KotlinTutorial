61c7d1ba976a701447481760fb676ac197d65f3d
Irfan Muhit <irfan.muhit85@gmail.com>
2022-06-03 05:35:16 +0600

    Object type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,16 @@
-// Lambdas are like anonymous functions. We can declare lambdas like this
-val quadruple = { number: Int -> number * 4}
+// 'object's are singleton. Only one instance is allowed. Objects don't have constructors
+object Screen {
+    val width = 1920
+    val height = 1080
 
-// We can also specify input and output type like this. If we do so, we don't hae to specify input
-// type in the function body
-val triplePower: (Int) -> Int = {number -> number * number * number}
-
-// A higher order function is such a function that takes a lambda as one of its parameters
-fun printStuff(foo: (Int) -> Int, input: Int, times: Int) {
-    // "repeat" is one of the many loops available in Kotlin
-    repeat(times) {
-        println(foo(input))
+    fun getResolution(): String {
+        return "$width x $height"
     }
 }
+
 fun main(args: Array<String>) {
-    println("Quadruple of 2 is: ${quadruple(2)}")
-    println("Triple power of 4 is: ${triplePower(4)}")
+    // Note that we don't call any constructors while instantiating objects because there isn't any
+    val screen = Screen;
 
-    println("#########################")
-
-    printStuff(quadruple, 5, 4)
-
-    println("#########################")
-
-    printStuff(triplePower, 5, 6)
-
+    println(screen.getResolution())
 }
