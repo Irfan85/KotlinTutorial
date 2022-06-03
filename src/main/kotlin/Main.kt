138ce19fc2cf3d6a00c7825ebbec076df7cf455c
Irfan Muhit <irfan.muhit85@gmail.com>
2022-06-03 19:19:55 +0600

    Pair, triple, list partition, filer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
-data class User(var firstName: String, var lastName: String, var email: String)
-
 fun main(args: Array<String>) {
-    val user1 = User("Akkas", "Ali", "akkasali@gmail.com")
-
-    // While destucturing, if a variable is not needed, an underscore ('-') can be placed in the
-    // place of that variable to skip it
-    val (a, _, c) = user1
-
+    // Pairs can be created using the 'to' keyword
+    val a = "Red" to "Blue"
     println(a)
-    println(c)
+    println("First: ${a.first}, Second: ${a.second}")
+
+    // Kotlin also provides triples
+    val b = Triple("Red", "Blue", "Green")
+    println(b)
+
+    var numbers = listOf<Int>(1, 2, 3, 4, 5, 6, 7)
+
+    // The partition function will divide the map based on the provided boolean predicate
+    var dividedLists = numbers.partition { it > 4 }
+    println(dividedLists.first)
+    println(dividedLists.second)
+
+    // Lists also have a filter function to fetch certain items
+    var filteredList = numbers.filter { it > 4 }
+    println(filteredList)
+
 }
