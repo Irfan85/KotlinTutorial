c08c07252d39e06043b97e4cb57d3ae4313be3bd
Irfan Muhit <irfan.muhit85@gmail.com>
2022-06-04 09:28:40 +0600

    'In' keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,25 @@
 open class Shape(open val width: Int, open val height: Int)
 
-open class ColourfulShape(width: Int, height: Int, color: String): Shape(width, height)
+open class ColourfulShape(width: Int, height: Int, val color: String): Shape(width, height)
 
-// By default, the type of generic will be <T: Any?> if we don't explicitly provide any type. The code would
-// look like this
-// class ShapeBox<T>(vararg val shape: T)
+// 'in' keyword checks type safety for parameters that are only taken as input for any function
+interface ShapePrinter<in T: Shape> {
+    fun printShape(shape : T)
+}
 
-// The vararg parameter will be treated as an array of type T
+class MyShapePrinterImpl: ShapePrinter<ColourfulShape>{
+    override fun printShape(shape: ColourfulShape) {
+        println("Shape width: ${shape.width}")
+        println("Shape height: ${shape.height}")
+        println("Shape color: ${shape.color}")
+    }
+}
 
-// The out type check is necessary if we want to check the type of variables that can be returned
+
 class ShapeBox<out T : Shape>(private vararg val shapes: T) {
-    fun printShapes() {
-        for (shape in shapes){
-            // This check function is provided by kotlin for assertion. If the boolean parameter is true, the
-            // assertion is valid and exception won't be thrown
-            check(!(shape.width == 0 || shape.height == 0)) {"ERROR: Invalid shape detected"}
-
-            println("Found: $shape")
+    fun printShapes(printer: ShapePrinter<T>) {
+        for (shape in shapes) {
+            printer.printShape(shape)
         }
     }
 }
@@ -31,14 +34,8 @@ fun main(args: Array<String>) {
     val colourfulShape2 = ColourfulShape(500, 500, "Blue")
     val colourfulShape3 = ColourfulShape(640, 480, "Green")
 
-    // We could also write ShapeBox<ColourfulShape> on the right  side. But it's not necessary in kotlin
-    // because of automatic type inference
     val myShapeBox = ShapeBox(colourfulShape1, colourfulShape2, colourfulShape3)
 
-    myShapeBox.printShapes()
-
-    println("#########################")
-
-    // If we didn't add the out keyword in the generic declaration, this would throw error
-    addItemTo(myShapeBox)
+    val shapePrinter = MyShapePrinterImpl()
+    myShapeBox.printShapes(shapePrinter)
 }
